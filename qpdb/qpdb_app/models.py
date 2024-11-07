2571ca50e29e9d84a1517450c47fc7a1a428c832
jacobdgm <jacob.4ristotle@gmail.com>
2024-11-07 11:47:44 -0500

    Add __str__ and __repr__ methods to Tune model

--- qpdb/qpdb_app/models.py
+++ qpdb/qpdb_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 class Tune(models.Model):
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return f"{self.name} ({self.id})"
+
     name = models.CharField(
         max_length=255,
         blank=False,
