d7ef4a5e93f313a5a86f1bef68fdbb14e20b4afc
jacobdgm <jacob.4ristotle@gmail.com>
2024-11-07 11:42:46 -0500

    Create and register Tune model

--- qpdb/qpdb_app/admin.py
+++ qpdb/qpdb_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Tune
 
-# Register your models here.
+admin.site.register(Tune)
+
--- qpdb/qpdb_app/models.py
+++ qpdb/qpdb_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Tune(models.Model):
+    name = models.CharField(
+        max_length=255,
+        blank=False,
+    )
+    type = models.CharField(
+        max_length=255,
+        blank=True,
+    )
+    key = models.CharField(
+        max_length=255,
+        blank=True,
+    )
+    composer = models.CharField(
+        max_length=255,
+        blank=True,
+    )
+    notes = models.TextField(
+        blank=True,
+    )
+    
