b7f6a56c367428c793663efc42b02dbb8a516050
jacobdgm <jacob.4ristotle@gmail.com>
2024-11-07 16:46:28 -0500

    Set up 'urls.py's and basic homepage view

--- qpdb/qpdb_app/views.py
+++ qpdb/qpdb_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+index_html = """
+    <title>
+        The Quatre Patates Database
+    </title>
+    <body>
+    <h1>
+        The Quatre Patates Database
+    </h1>
+    <p>Welcome to the Quatre Patates Database!
+"""
+
+def index(request):
+    return HttpResponse(index_html)
